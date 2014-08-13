20a0edfb46c9028f348ad30885902b54fc05e785
yakulten <capsuleyo@gmail.com>
2014-08-13 23:19:28 +0900

    organized applicatino.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,7 +6,6 @@
 //= require angular-strap/dist/angular-strap
 //= require highcharts-ng/dist/highcharts-ng
 //= require ng-table/ng-table
-//= require angular.treeview/angular.treeview
 //= require angular-ui-router
 //= require angular-ui-calendar
 //= require select2
